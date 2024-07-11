d003952ea87f038d3f70d77e27e074d1c31d053f
Charlespm214 <charlesthebest@gmail.com>
2024-07-11 18:52:07 -0500

    Modulo_Contacto
    Conversión campos del formulario de contacto a cadena texto.

--- Javascript/contacto.js
+++ Javascript/contacto.js
@@ -10,46 +10,47 @@ cerrar.addEventListener('click', ()=>
         modal.classList.remove('modal--Mostrar')      
     })
 
-// function borrarForm()
-//     {
-//         document.addEventListener('DOMContentLoaded',function()
-//             {
-//                 var botonBorrar=document.getElementById('btnBorrar');
-//                 botonBorrar.addEventListener('click',function()
-//                     {
-//                         document.getElementById('names').value="";
-//                         document.getElementById('email').value="";
-//                         document.getElementById('celular').value="";
-//                         document.getElementById('mensaje').value="";
-//                     })
-//             })
-//     }
-// borrarForm()
+function borrarForm()
+    {
+        document.addEventListener('DOMContentLoaded',function()
+            {
+                var botonBorrar=document.getElementById('btnBorrar');
+                botonBorrar.addEventListener('click',function()
+                    {
+                        document.getElementById('names').value="";
+                        document.getElementById('email').value="";
+                        document.getElementById('celular').value="";
+                        document.getElementById('mensaje').value="";
+                    })
+            })
+    }
+borrarForm()
 // ------------------------
+function enviarForm()
+    {
         let names;
         let email;
         let celular;
         let mensaje;
         let texto;
-function enviarForm()
-    {
+
         document.addEventListener('DOMContentLoaded', function()
             {
                 var botonEnviar=document.getElementById('btnEnviar');
                 botonEnviar.addEventListener('click', function()
                     {
-                        // names=document.getElementById('names').value
-                        // email=document.getElementById('email').value
-                        // celular=document.getElementById('celular').value
-                        // mensaje=document.getElementById('mensaje').value
+                        names=document.getElementById('names').value
+                        email=document.getElementById('email').value
+                        celular=document.getElementById('celular').value
+                        mensaje=document.getElementById('mensaje').value
                     
                             document.getElementById('names').value=names;
                             document.getElementById('email').value=email;
                             document.getElementById('celular').value=celular;
                             document.getElementById('mensaje').value=mensaje;
     
-                    let cel=celular.toString()
-                    texto = ("Nombre: "+names+" "+"Email: "+email+" "+"Celular: "+cel+" "+"Mensaje: "+mensaje)
+                    // let cel=celular.toString()
+                    texto = ("Nombre: "+names+" "+"Email: "+email+" "+"Celular: "+(celular).toString()+" "+"Mensaje: "+mensaje)
                     console.log(texto)
                     alert(texto)
                     })
